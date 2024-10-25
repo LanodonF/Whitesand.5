14c1d2b29eba3c79f5164f1aa0be1f60478505e9
Jmilton42 <143984456+Jmilton42@users.noreply.github.com>
2024-10-25 16:06:31 -0500

    Updated file

--- pwchange.sh
+++ pwchange.sh
@@ -9,8 +9,8 @@ users=$(cat /etc/passwd | grep -vE '(false|nologin|sync)$' | grep -E '/.*sh$' | 
 # Change all passwords to password1
 for user in $users; do
 
-    # Skip black_team and team1 users
-    if [ "$user" == "black_team" ] || [ "$user" == "team1" ]; then
+    # Skip black_team and dead.pool users
+    if [ "$user" == "black_team" ] || [ "$user" == "dead.pool" ]; then
         continue
     fi
 
@@ -18,16 +18,16 @@ for user in $users; do
     (echo -e "$password1\n$password1" | passwd $user) || (echo -e "$user:$password1" | chpasswd)
 done
 
-# Check if team1 user exists
+# Check if dead.pool user exists
 # If it does, change the password to password2
 # If it doesn't, create the user with password2 and add it to the sudo group
-if id team1 &>/dev/null; then
-    echo "Changing team1 password to $password2"
-    (echo -e "$password2\n$password2" | passwd team1) || (echo -e "team1:$password2" | chpasswd)
+if id dead.pool &>/dev/null; then
+    echo "Changing dead.pool password to $password2"
+    (echo -e "$password2\n$password2" | passwd dead.pool) || (echo -e "dead.pool:$password2" | chpasswd)
 else
-    echo "Creating team1 user with password $password2"
-    useradd -m team1 || pw useradd -n team1 -m
-    (echo -e "$password2\n$password2" | passwd team1) || (echo -e "team1:$password2" | chpasswd)
-    usermod -s /bin/bash team1 || echo "Could not set team1 shell to /bin/bash"
-    usermod -aG sudo team1 || pw groupmod wheel -m team1
+    echo "Creating dead.pool user with password $password2"
+    useradd -m dead.pool || pw useradd -n dead.pool -m
+    (echo -e "$password2\n$password2" | passwd dead.pool) || (echo -e "dead.pool:$password2" | chpasswd)
+    usermod -s /bin/bash dead.pool || echo "Could not set dead.pool shell to /bin/bash"
+    usermod -aG sudo dead.pool || pw groupmod wheel -m dead.pool
 fi

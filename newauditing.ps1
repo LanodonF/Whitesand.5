f4a75aad2c038962fbfb8471b213052da6738857
Landon <foister150@student.pcsstn.net>
2024-10-25 21:09:01 -0500

    Added add audit and starting needs

---
+++ newauditing.ps1
@@ -0,0 +1,62 @@
+﻿$admincsv = import-csv -Path "C:\Users\$env:USERNAME\Documents\admin.csv" | Select-Object -First 9
+$usercsv = import-csv -Path "C:\Users\$env:USERNAME\Documents\admin.csv" | Select-Object -Skip 9
+
+
+# Get AD-Users from windows, selects the accounts that are NOT the built in Administrator, Guest, Windows Defender Account, the Default user account, and the Kerberos account. Outputs a hashtable in 
+$currentUsers = get-aduser -filter * | Where-Object {$_.Name -ne "Administrator" -and $_.Name -ne "Guest" -and $_.Name -ne "WDAGUtilityAccount" -and $_.Name -ne "DefaultAccount" -and $_.Name -ne "krbtgt" -and $_.Name -ne "black_team" } | select SamAccountName 
+
+# Getting the currentUsers out of the hash table
+$currentUsers = $currentUsers | ForEach-Object { $_.SamAccountName }
+
+
+# log users to remove
+$currentUsers | Where-Object { $_ -notin $usercsv.ForEach{$_.username} -and $_ -notin $admincsv.ForEach{$_.username} } | ForEach-Object{Add-Content -Path "C:\Users\$env:USERNAME\Documents\DelUser.txt" -Value "$_"}
+
+# Finding and deleting unauthorized users
+$currentUsers | Where-Object { $_ -notin $usercsv.ForEach{$_.username} -and $_ -notin $admincsv.ForEach{$_.username} } | ForEach-Object {Remove-ADUser -Identity $_}
+
+
+# Log users to add
+$admincsv | Where-Object { $_.username -notin $currentUsers } | ForEach-Object{
+Add-Content -Path "C:\Users\$env:USERNAME\Documents\AddUser.txt" -Value "$($_.username)"
+}
+
+# Log users to add
+$usercsv | Where-Object { $_.username -notin $currentUsers } | ForEach-Object{
+Add-Content -Path "C:\Users\$env:USERNAME\Documents\AddUser.txt" -Value "$($_.username)"
+}
+
+$userPassword = ConvertTo-SecureString "team1Adm!n!" -AsPlainText -Force
+$adminPassword = ConvertTo-SecureString "team1Adm!n!" -AsPlainText -Force
+
+$usercsv | Where-Object { $_.username -notin $currentUsers } | ForEach-Object{New-ADUser -Name $_.username -AccountPassword $userPasssword -Enabled $true}
+$admincsv | Where-Object { $_.username -notin $currentUsers } | ForEach-Object{New-ADUser -Name $_.username -AccountPassword $adminPasssword -Enabled $true}
+
+
+# Get AD-Users from windows, selects the accounts that are NOT the built in Administrator, Guest, Windows Defender Account, the Default user account, and the Kerberos account. Outputs a hashtable in 
+$currentUsers = get-aduser -filter * | Where-Object {$_.Name -ne "Administrator" -and $_.Name -ne "Guest" -and $_.Name -ne "WDAGUtilityAccount" -and $_.Name -ne "DefaultAccount" -and $_.Name -ne "krbtgt" -and $_.Name -ne "black_team" } | select SamAccountName 
+
+# Getting the currentUsers out of the hash table
+$currentUsers = $currentUsers | ForEach-Object { $_.SamAccountName }
+
+
+#Make sure password stored using reversable encryption off
+$currentUsers | ForEach-Object {Set-ADUser -Identity $_ -AllowReversiblePasswordEncryption $false}
+
+# Make sure all authorized accounts are enabled
+$currentUsers | ForEach-Object {Enable-ADAccount $_}
+
+#Accounts not for delegation
+$currentUsers | ForEach-Object {Set-ADUser -Identity $_ -AccountNotDelegated $true}
+
+#Disable do not require kerberos preauth
+$currentUsers | ForEach-Object {Set-ADAccountControl -Identity $_ -DoesNotRequirePreAuth $false}
+
+#Account Delegation
+$currentUsers | ForEach-Object {Set-ADAccountControl -Identity $_ -AccountNotDelegated $true}
+
+#Enable accounts
+$currentUsers | ForEach-Object {Set-ADAccountControl -Identity $_ -Enabled $true}
+
+#Deleting sID history
+$currentUsers | ForEach-Object {Set-ADUser $_ -remove @{sidhistory=$_.sidhistory.value}}
---
+++ start.ps1
@@ -0,0 +1,14 @@
+﻿# Shellbags
+# Windows scripts
+
+
+
+ 
+# Wireshark
+Invoke-WebRequest https://2.na.dl.wireshark.org/win64/Wireshark-4.4.1-x64.exe -OutFile "Wireshark.exe"
+
+#Sysinternals
+Invoke-WebRequest https://download.sysinternals.com/files/SysinternalsSuite.zip -OutFile "Sysinternals.zip"
+Expand-Archive -Path "Sysinternals.zip"
+
+Invoke-WebRequest "https://objects.githubusercontent.com/github-production-release-asset-2e65be/23216272/b956bf27-aac4-4674-bcf3-bb4ff2e61e22?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20241026%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241026T014651Z&X-Amz-Expires=300&X-Amz-Signature=43c140188e76e6173c598f852c7bacb333ef9ee41f1ddb7f94893fd01ec9bdbf&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3DGit-2.47.0-64-bit.exe&response-content-type=application%2Foctet-stream" -OutFile "gitinstall.exe"
